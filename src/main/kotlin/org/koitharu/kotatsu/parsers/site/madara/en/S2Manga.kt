55c29ac779b101484be4008cbb3dbd27873b2e92
AlishaShahban <137408468+AlishaShahban@users.noreply.github.com>
2026-05-28 10:20:56 +0100

    Update S2Manga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/S2Manga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/S2Manga.kt
@@ -8,33 +8,17 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 @MangaSourceParser("S2MANGA", "S2Manga", "en")
 internal class S2Manga(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.S2MANGA, "s2read.com") {
+        
+    override val datePattern = "MMMM dd, yyyy"
 
-    override suspend fun getChapters(manga: Manga, doc: Document): List<MangaChapter> {
-		val slug = manga.url.removeSuffix('/').substringAfterLast('/')
-		val doc2 = webClient.httpPost(
-			"https://$domain/manga/$slug/ajax/chapters/",
-			mapOf(),
-		).parseHtml()
-		val ul = doc2.body().selectFirstOrThrow("ul")
-		val dateFormat = SimpleDateFormat(datePattern, Locale.US)
-		return ul.select("li").mapChapters(reversed = true) { i, li ->
-			val a = li.selectFirst("a")
-			val href = a?.attrAsRelativeUrlOrNull("href") ?: li.parseFailed("Link is missing")
-			MangaChapter(
-				id = generateUid(href),
-				title = a.ownText(),
-				number = i + 1f,
-				volume = 0,
-				url = href,
-				uploadDate = parseChapterDate(
-					dateFormat,
-					li.selectFirst("span.chapter-release-date i")?.text(),
-				),
-				source = source,
-				scanlator = null,
-				branch = null,
-			)
-		}
+    // Scenario 1 code is pasted here
+    override fun getHeaders(url: String): Headers {
+        return super.getHeaders(url).newBuilder().apply {
+            add("X-Requested-With", "XMLHttpRequest")
+            add("Referer", "https://s2read.com/")
+        }.build()
+    }
+
 	}
 
 }
