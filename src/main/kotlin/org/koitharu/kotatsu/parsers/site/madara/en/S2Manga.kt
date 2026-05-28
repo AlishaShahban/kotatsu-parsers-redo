b0ae6a5486c7aa27de36cbb27dd70bfac97f4544
AlishaShahban <137408468+AlishaShahban@users.noreply.github.com>
2026-05-28 14:59:33 +0100

    Update S2Manga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/S2Manga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/S2Manga.kt
@@ -1,7 +1,9 @@
 package org.koitharu.kotatsu.parsers.site.madara.en
 
+import org.jsoup.nodes.Document // Required for Document
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.MangaChapter // Required for MangaChapter
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
@@ -10,6 +12,17 @@ internal class S2Manga(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.S2MANGA, "s2read.com") {
 	
     override val datePattern = "MMMM dd, yyyy"
-    
-    // Tells the parser to use admin-ajax.php with the manga ID instead of the broken /ajax/chapters/ URL
-    override val postReq = true
+
+    // Intercepts the broken mangaUrl caused by S2Read's bad SEO tags
+    override suspend fun loadChapters(mangaUrl: String, document: Document): List<MangaChapter> {
+        // If the URL was stripped down to just the root, use the actual document URL instead
+        val correctUrl = if (mangaUrl == "/" || mangaUrl.isEmpty()) {
+            document.baseUri()
+        } else {
+            mangaUrl
+        }
+        
+        // Pass the corrected URL back to the default Madara ajax handler
+        return super.loadChapters(correctUrl, document)
+    }
+}
